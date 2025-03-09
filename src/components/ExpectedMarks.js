cf1763badf4b7eea654fa90f05d0e9b6e8fd163a
Dudekula Rahim <119217092+DUDEKULARAHIM@users.noreply.github.com>
2025-03-09 22:21:04 +0530

    Update ExpectedMarks.js

--- src/components/ExpectedMarks.js
+++ src/components/ExpectedMarks.js
@@ -1,6 +1,4 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import "./ExpectedMarks.css";
+
 
 // const ExpectedMarks = () => {
 //   const [courseCode, setCourseCode] = useState("");
